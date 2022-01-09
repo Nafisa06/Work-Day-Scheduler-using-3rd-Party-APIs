8d3c4bcc97602b4ea20d9c5502bfdf2d179bced8
Nafisa06 <nafisa.ila@gmail.com>
2022-01-10 01:45:18 +1100

    changed date format from US format to Australian format

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,3 @@
 // Add today's date to jumbotron using moment.js
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd, MMM Do YYYY"));
